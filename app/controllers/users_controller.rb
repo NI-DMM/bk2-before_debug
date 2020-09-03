19cc969540f889a6fbf7a9841d3edfec933323f0
vagrant <vagrant@localhost.localdomain>
2020-09-03 10:32:28 +0000

    Finished

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+    redirect_to user_path(current_user.id), alert: '不正なアクセスです。'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find_by(id:params[:id])
+    unless @user.id == current_user.id
+      redirect_to user_path(@user)
+    end
+  end
+
+end
